e428611c0e5471543cb2c7068ba26b9fa17b4b3d
Leonardo Sabino <leonardosabinoti@gmail.com>
2019-07-28 20:37:08 -0300

    Adição de Programas

--- Instalação de Pacotes Linux/Instala-Pacotes.sh
+++ Instalação de Pacotes Linux/Instala-Pacotes.sh
@@ -26,7 +26,7 @@ case $opcao in
     1)
     while ! [ "$Config" == "Y" -o  "$Config" == "y" -o "$Config" == "N" -o "$Config" == "n" ]
     do
-      echo -e "Será criada uma pasta temporária para auxiliar na instalação dos programas:\nGIT/ATOM/ZSH/CURL/DOCKER/GIMP/VLC/GOOGLE CHROME/LIBRE OFFICE/SNAP/VSCODE\n Podemos continuar (Y/N)?"
+      echo -e "Será criada uma pasta temporária para auxiliar na instalação dos programas:\nGIT/ATOM/ZSH/CURL/DOCKER/DOCKER-COMPOSE/GIMP/VLC/GOOGLE CHROME/LIBRE OFFICE/SNAP/VSCODE/PHPSTORM\n Podemos continuar (Y/N)?"
       read Config
       if [ "$Config" == "Y" -o "$Config" == "y" ]
         then
@@ -53,66 +53,61 @@ case $opcao in
       done
 
       echo Atualizando repositórios..
-      if ! apt-get update
+      if ! sudo apt-get update
       then
-        echo "Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list"
         exit 1
       fi
       echo "Atualização feita com sucesso"
 
       echo "Atualizando pacotes já instalados"
-      if ! apt-get dist-upgrade -y
+      if ! sudo apt-get dist-upgrade -y
       then
-        echo "Não foi possível atualizar pacotes."
-        echo "Por favor revise seu comando"
         exit 1
       fi
       echo "Atualização de pacotes feita com sucesso"
 
       #Instalando o programa Git
 
-      if ! apt-get install git
+      if ! sudo apt-get install git -y
       then
-        echo "Não foi possível instalar o pacote Git"
-        echo "Por favor revise seu comando"
         ErrorGit= "Git,"
       fi
       echo "Instalação do Git foi finalizada"
 
       #Instalando o programa Snap
 
-      if ! sudo apt-get install snapd
+      if ! sudo apt-get install snapd -y
       then
-        echo "Não foi possível instalar o pacote Snap"
-        echo "Por favor revise seu comando"
         ErrorSnap=" Snap,"
       fi
       echo "Instalação do Snap foi finalizada"
 
       #Instalando o programa Atom
-
-      if sudo apt-get update
-      then
-        if ! sudo apt-get install atom
+      if sudo add-apt-repository ppa:webupd8team/atom -y
         then
-          echo "Não foi possível instalar o pacote ATOM"
-          echo "Por favor revise seu comando"
+          if sudo apt-get update
+          then
+            if ! sudo apt-get install atom -y
+            then
+              ErrorAtom=" Atom,"
+            fi
+          else
+            ErrorAtom=" Atom,"
+          fi
+        else
           ErrorAtom=" Atom,"
-        fi
-      else
-        echo "Não foi possível atualizar os repositórios. Verifique seu arquivo /etc/apt/sources.list"
       fi
       echo "Instalação do ATOM foi finalizada"
 
       #Instalando o Shell ZSH e o Framework oh-my-ZSH
 
-      if sudo apt-get install zsh
+      if sudo apt-get install zsh -y
         then
           if sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
           then
-            if git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+            if git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
             then
-              if ! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+              if ! git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
               then
                 echo "Não foi possível instalar o Plugin de compreender a sixtaxe das linguagens do Bash ZSH"
                 echo "Por favor revise seu comando"
@@ -140,7 +135,7 @@ case $opcao in
 
       if sudo apt-get update
       then
-        if sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+        if sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common-y
         then
           if curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
           then
@@ -151,8 +146,13 @@ case $opcao in
               then
                 if sudo apt-get update
                 then
-                  if ! sudo apt-get install docker-ce docker-ce-cli containerd.io
+                  if sudo apt-get install docker-ce docker-ce-cli containerd.io -y
                   then
+                    if ! sudo usermod -aG docker $(whoami) # Comando para deixar o docker sem sudo -y
+                    then
+                      ErrorDocker=" Docker,"
+                    fi
+                    else
                     echo "Não foi possível instalar a ultima versão do Docker no seu Pc"
                     echo "Por favor revise seu comando"
                     ErrorDocker=" Docker,"
@@ -187,13 +187,26 @@ case $opcao in
         fi
       echo "Instalação do Docker foi finalizada"
 
+      #Instalando Docker-compose
+
+      if sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose -y
+        then
+          if ! sudo chmod +x /usr/local/bin/docker-compose -y
+            then
+              ErrorDockerCompose=" Docker-Compose,"
+          fi
+        else
+          ErrorDockerCompose=" Docker-Compose,"
+      fi
+
+
       #Instalando o Gimp
 
-      if sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+      if sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
       then
         if sudo apt-get update
         then
-          if ! sudo apt-get install -y gimp
+          if ! sudo apt-get install gimp -y
           then
             echo "Não foi possível instalar a ultima versão do Gimp no seu Pc"
             echo "Por favor revise seu comando"
@@ -212,7 +225,7 @@ case $opcao in
 
       # Instalar o VLC
 
-      if ! sudo apt-get install vlc
+      if ! sudo apt-get install vlc -y
       then
         echo "Não foi possível instalar o pacote Vlc"
         echo "Por favor revise seu comando"
@@ -224,7 +237,7 @@ case $opcao in
 
       if wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
       then
-        if ! sudo gdebi google-chrome-stable_current_amd64.deb
+        if ! sudo gdebi google-chrome-stable_current_amd64.deb -y
         then
           echo "Não foi possível baixar o pacote do Google Chorome"
           echo "Por favor revise seu comando"
@@ -237,19 +250,11 @@ case $opcao in
       fi
       echo "Instalação do Google Chorome foi finalizada"
 
-      if ! snap install code --classic
-      then
-        echo "Não foi possível instalar o pacote "
-        echo "Por favor revise seu comando"
-        ErrorVSCode=" Visual Studio Code"
-      fi
-      echo "Instalação do Visual Studio Code foi finalizada"
-
       if ErrorZsh==""
       then
         echo "Todas as instalações foram finalizadas deseja configurar o ZSH? (Y/N)"
-        read Config
-        if $Config -eq Y -o $Config -eq Y
+        read Configs
+        if [ "$Configs" == "Y" -o "$Configs" == "y" ]
         then
           if ! [ -a ConfiguraçãoZsh.txt ]
           then
@@ -257,11 +262,16 @@ case $opcao in
             echo -e "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)\n" >ConfiguraçãoZsh.txt
             echo -e "(composer,copyfile,dirhistory,django,docker,extract,git,ng,node,npm,python,sudo)\n" >ConfiguraçãoZsh.txt
             echo -e "#ZSH_THEME="blinks" (agnoster ou blinks ou pygmalion ou wezm)\n" >ConfiguraçãoZsh.txt
-            nano ConfiguraçãoZsh
-            nano ~/.zshrc
+            atom ConfiguraçãoZsh
+            atom ~/.zshrc
           else
             echo ""
             exit 1
+          fi
+        else
+          if [ "$Configs" == "N" -o "$Configs" == "n" ]
+            then
+              exit 1
           fi
         fi
       fi
@@ -282,3 +292,42 @@ case $opcao in
         echo;;
 esac
 done
+
+
+
+
+#PhpStorm Editor
+
+wget https://download.jetbrains.com/webide/PhpStorm-2019.2.tar.gz -O phpstorm.tar.gz
+sudo tar vzxf phpstorm.tar.gz -C /opt/
+sudo mv /opt/PhpStorm*/ /opt/phpstorm
+sudo ln -sf /opt/phpstorm/bin/phpstorm.sh /usr/bin/phpstorm
+echo -e '[Desktop Entry]\n Version=1.0\n Name=phpstorm\n Exec=/opt/phpstorm/bin/phpstorm.sh\n Icon=/opt/phpstorm/bin/phpstorm.png\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/phpstorm.desktop
+sudo chmod +x /usr/share/applications/phpstorm.desktop
+cp /usr/share/applications/phpstorm.desktop ~/Desktop #Em Ingles
+cp /usr/share/applications/phpstorm.desktop  ~/Área\ de\ Trabalho/ #Em Português
+
+#mpv Player
+
+sudo add-apt-repository ppa:mc3man/mpv-tests
+sudo apt-get install mpv
+
+#Visual Code Studio
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+#Sublime
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+#Brackets
+
+sudo add-apt-repository ppa:webupd8team/brackets
+sudo apt-get update
+sudo apt-get install brackets
